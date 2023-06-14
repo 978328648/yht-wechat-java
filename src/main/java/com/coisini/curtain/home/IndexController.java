6b75e265cff3e82d10091e847b5ee31ce9247c4a
嘿嘿 <lyt01154983@cainiao.com>
2023-06-14 19:21:38 +0800

    feat: 获取banner图路径

--- src/main/java/com/coisini/curtain/home/IndexController.java
+++ src/main/java/com/coisini/curtain/home/IndexController.java
@@ -1,11 +1,14 @@
 package com.coisini.curtain.home;
 
+import com.coisini.curtain.sql.DatabaseUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +18,25 @@ import java.util.Map;
 @SpringBootApplication
 @RequestMapping("/user/*")
 public class IndexController {
-
+    @RequestMapping("/getHomeBanner")
+    public Map<String,Object> getHomeBanner() throws SQLException {
+        System.out.println("getHomeBanner正在调用...");
+        Map<String,Object> map = new HashMap<String, Object>();
+        List<String> userList = new ArrayList<>();
+        String sql = "SELECT * FROM wechat_bannerimg WHERE url";
+        ResultSet rs = DatabaseUtil.executeQuery(sql);
+        System.out.println("111122223333");
+        System.out.println(rs.next());
+        while (rs.next()) {
+            String url = rs.getString("url");
+//            int userAge = rs.getInt("age");
+//            User user = new User(url);
+            userList.add(url);
+        }
+        map.put("imgList",userList);
+        System.out.println("getHomeBanner调用完成...");
+        return map;
+    }
     @RequestMapping("/getUser")
     public Map<String,Object> getUser(){
         System.out.println("微信小程序正在调用...");
---
+++ src/main/java/com/coisini/curtain/sql/DatabaseUtil.java
@@ -0,0 +1,34 @@
+package com.coisini.curtain.sql;
+
+import java.sql.*;
+
+/**
+ * @author liu
+ * 该代码定义了一个名为DatabaseUtil的公共类，
+ * 其中包含一个名为executeQuery的公共方法。
+ * 该方法接受一个SQL查询语句，以及可变数量的查询参数，
+ * 并通过JDBC API执行查询操作。在方法中，
+ * 先使用JDBC API获取数据库连接，再通过PreparedStatement类设置查询参数，
+ * 最后执行查询操作，并返回查询结果集ResultSet。
+ * 该方法可以在其他Java类中调用，以实现数据库查询的代码复用。
+ */
+public class DatabaseUtil {
+    private static final String URL = "jdbc:mysql://www.a57521.com/curtain?characterEncoding=UTF-8&useSSL=false&serverTimezone=GMT%2B8&allowPublicKeyRetrieval=true";
+    private static final String USER = "curtain";
+    private static final String PASSWORD = "demaxiya789.";
+
+    public static ResultSet executeQuery(String sql, Object... params) throws SQLException {
+        Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        int index = 1;
+        for (Object param : params) {
+            pstmt.setObject(index, param);
+            index++;
+        }
+
+        ResultSet rs = pstmt.executeQuery();
+        return rs;
+    }
+}
+
